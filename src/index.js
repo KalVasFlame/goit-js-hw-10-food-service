ab84a3fd8e51fc7a48bdecfb66f3659c44ea0a35
KalVasFlame <unnameddwar@gmail.com>
2021-02-24 04:45:58 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './styles.css';
+  const Handlebars = require("handlebars");
+import dishesList from './menu.json'
+import itemsTemplate from './templates/menu-items.hbs'
+
+const markup = itemsTemplate(dishesList) 
+// const menuRef = document.querySelector('.js-menu');
+console.log(document);
+
+console.log(Handlebars);
+menuRef.insertAdjacentHTML('beforeend', markup);
