21c71530b9c22561d6f89c632606b26474e40ec6
KalVasFlame <unnameddwar@gmail.com>
2021-02-25 13:40:58 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-  const Handlebars = require("handlebars");
-import dishesList from './menu.json'
-import itemsTemplate from './templates/menu-items.hbs'
 
-const markup = itemsTemplate(dishesList) 
-// const menuRef = document.querySelector('.js-menu');
-console.log(document);
-
-console.log(Handlebars);
-menuRef.insertAdjacentHTML('beforeend', markup);
+import './styles.css';
+import './js/templating.js';
+import './js/theme.js'
---
+++ src/js/theme.js
@@ -0,0 +1,33 @@
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const bodyRef = document.querySelector('body');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+
+
+const onCheckboxChange = () => { 
+  if (checkboxRef.checked) {
+    localStorage.setItem('theme', Theme.DARK)
+    bodyRef.classList.remove(Theme.LIGHT)
+    bodyRef.classList.add(Theme.DARK)
+  } else {
+    localStorage.setItem('theme', Theme.LIGHT)
+    bodyRef.classList.remove(Theme.DARK)
+    bodyRef.classList.add(Theme.LIGHT)
+  }
+}
+
+const  saveSettings = () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === Theme.DARK) {
+    checkboxRef.checked = 'true';
+    onCheckboxChange();
+  }
+}
+
+
+checkboxRef.addEventListener('change', onCheckboxChange)
+saveSettings()
