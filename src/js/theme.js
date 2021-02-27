9e34911e163e267618fa6d782f212ff9bbd4310f
KalVasFlame <unnameddwar@gmail.com>
2021-02-27 19:52:04 +0200

    +

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,10 +6,8 @@ const Theme = {
 const bodyRef = document.querySelector('body');
 const checkboxRef = document.querySelector('#theme-switch-toggle');
 
-
-
-const onCheckboxChange = () => { 
-  if (checkboxRef.checked) {
+const switchTheme = ({ LIGHT, DARK }) => { 
+if (checkboxRef.checked) {
     localStorage.setItem('theme', Theme.DARK)
     bodyRef.classList.remove(Theme.LIGHT)
     bodyRef.classList.add(Theme.DARK)
@@ -18,6 +16,19 @@ const onCheckboxChange = () => {
     bodyRef.classList.remove(Theme.DARK)
     bodyRef.classList.add(Theme.LIGHT)
   }
+}
+
+const onCheckboxChange = () => { 
+  switchTheme(Theme)
+  // if (checkboxRef.checked) {
+  //   localStorage.setItem('theme', Theme.DARK)
+  //   bodyRef.classList.remove(Theme.LIGHT)
+  //   bodyRef.classList.add(Theme.DARK)
+  // } else {
+  //   localStorage.setItem('theme', Theme.LIGHT)
+  //   bodyRef.classList.remove(Theme.DARK)
+  //   bodyRef.classList.add(Theme.LIGHT)
+  // }
 }
 
 const  saveSettings = () => {
